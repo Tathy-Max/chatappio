24d02e451ffbcabf254b43d547604e46eb769f40
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-08-26 19:22:31 -0400

    css adjustments

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import chatAppLogo from './assets/images/chatAppLogo.png';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 function Chat({ socket, username, room }) {
 	const [currentMessage, setCurrentMessage] = useState('');
@@ -24,6 +24,7 @@ function Chat({ socket, username, room }) {
 	useEffect(() => {
 		socket.on('receive_message', (data) => {
 			setMessageList((list) => [...list, data]);
+			setCurrentMessage('');
 		});
 	}, [socket]);
 
@@ -33,29 +34,36 @@ function Chat({ socket, username, room }) {
 				<p>Live Chat</p>
 			</div>
 			<div className="chat-body">
-				{messageList.map((messageContent) => {
-					return (
-						<div className="message">
-							<div>
-								<div className="message-content">
-									<p>{messageContent.message}</p>
-								</div>
-								<div className="message-meta">
-									<p>{messageContent.time}</p>
-									<p>{messageContent.author}</p>
+				<ScrollToBottom className="message-container">
+					{messageList.map((messageContent) => {
+						return (
+							<div
+								className="message"
+								id={username === messageContent.author ? 'you' : 'other'}
+							>
+								<div>
+									<div className="message-content">
+										<p>{messageContent.message}</p>
+									</div>
+									<div className="message-meta">
+										<p id="time">{messageContent.time}</p>
+										<p id="author">{messageContent.author}</p>
+									</div>
 								</div>
 							</div>
-						</div>
-					);
-				})}
+						);
+					})}
+				</ScrollToBottom>
 			</div>
 			<div className="chat-footer">
 				<input
 					type="text"
+					value={currentMessage}
 					placeholder="Hey..."
 					onChange={(event) => {
 						setCurrentMessage(event.target.value);
 					}}
+					onKeyPress={(event) => event.key === 'Enter' && sendMessage()}
 				/>
 				<button onClick={sendMessage}>&#9658;</button>
 			</div>
